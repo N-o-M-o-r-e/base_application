cd8d43263f4d4a881b8ab18fd32d8aff7ddbbe6c
No~More <sn.tthanh000@gmail.com>
2025-06-22 16:59:31 +0700

    Sửa lỗi trùng lặp trong libs.versions.toml cho version 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.7"
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
